878a91b58743d367ef0b4a96fd1b0813c5e608eb
Steven Hoberg <steven.hoberg1@gmail.com>
2025-02-01 13:26:29 -0500

    prompting control

--- test.py
+++ test.py
@@ -4,14 +4,21 @@ from groq import Groq
 
 client = Groq(api_key = "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")
 
-chat_completion = client.chat.completions.create(
-    messages=[
-        {
-            "role": "user",
-            "content": "Explain the importance of fast language models",
-        }
-    ],
-    model="llama-3.3-70b-versatile",
-)
 
-print(chat_completion.choices[0].message.content)
+print("Whats up? enter to leave\n")
+request = input()
+while(request != ""):
+
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": request,
+            }
+        ],
+        model="llama-3.3-70b-versatile",
+    )
+    print(chat_completion.choices[0].message.content)
+    print("\nThats all. Anything else? enter to leave\n")
+    request = input()
+print("See ya later")
