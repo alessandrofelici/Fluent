af40e1e486b6de32f23ef022cc5385e41fe4cdf7
Alex Felici <sandroms521@gmail.com>
2025-02-01 15:28:37 -0500

    Revert "Textbox on Local (Alex)"
    This reverts commit be61d06177a4595ca0ae9fe2749c6a4578d3f011.

--- Fluent/src/App.tsx
+++ Fluent/src/App.tsx
@@ -1,20 +1,26 @@
 import { useState } from 'react'
 
-
 function App() {
+  const [value, setValue] = useState('');
   const [count, setCount] = useState(0)
 
   function handleClick() {
     setCount(count+1);
   }
 
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setValue(event.target.value);
+  }
+
   return (
-    <>
+    <div>
       <h1>Free protein</h1>
       <button type="button" onClick={() => handleClick()}>
         {count}g
       </button>
-    </>
+      <input type="text" value={value} onChange={(value) => handleChange(value)} />
+      <p>Value: {value}</p>
+    </div>
   )
 }
 
