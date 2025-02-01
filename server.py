a417b71e067fbe88b4ce0ae4025ec7d99517a654
vznn16 <asherr.k.ralph@gmail.com>
2025-02-01 15:57:28 -0500

    server.py

---
+++ server.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, jsonify
+import requests
+
+app = Flask(__name__)
+
+import os
+
+from groq import Groq
+
+def chatbot_response(request):
+
+    client = Groq(api_key = "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")
+    
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": request,
+            }            ],
+        model="llama-3.3-70b-versatile",
+    )
+    return (chat_completion.choices[0].message.content)
+        
+
+@app.route("/chat", methods=["POST"])
+def chat():
+    user_input = request.json.get("message")
+    bot_response = chatbot_response(user_input)
+    return jsonify({"response": bot_response})
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
