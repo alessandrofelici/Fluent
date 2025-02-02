81a41401c3c8ddb5051e30e5132661402a4829f1
Armando <zavalaa4@msu.edu>
2025-02-02 05:17:10 -0500

    Rid of home button in home

--- Fluent/src/pages/Home.tsx
+++ Fluent/src/pages/Home.tsx
@@ -5,7 +5,20 @@ import Header from '../Header';
 function Home() {
     return (
         <div>
-            <Header/>
+            <><div style={{
+                position: 'fixed',
+                top: 0,
+                left: 0,
+                right: 0,
+                backgroundColor: '#000',
+                padding: '10px',
+                boxShadow: '0 2px 4px rgba(112, 112, 112, 0.1)',
+                zIndex: 1000,
+            }}>
+            <h1>
+                Fluent !
+            </h1>
+            </div></>
             <h1>How would you like to study?</h1>
             <Link to="/Chat">
                 <button className="pages" style={{ padding: '10px 20px',
