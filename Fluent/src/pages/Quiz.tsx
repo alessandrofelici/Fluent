e1003d0bb9ec74742993f0e15b50148a89695f55
Alex Felici <sandroms521@gmail.com>
2025-02-02 01:02:51 -0500

    vercel capitalization change??

--- Fluent/src/pages/Quiz.tsx
+++ Fluent/src/pages/Quiz.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom'
-import Mcq from '../mcq';
+import Mcq from '../Mcq';
 import Header from '../Header';
 
 function Quiz() {
