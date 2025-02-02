ed080aaa7730910ffadb9ce24ab418ab8d685401
Alex Felici <sandroms521@gmail.com>
2025-02-02 02:32:26 -0500

    Base Flashcard function hardcoded

--- Fluent/src/pages/Flashcards.jsx
+++ Fluent/src/pages/Flashcards.jsx
@@ -1,12 +1,27 @@
 import Header from '../Header';
+import { useState } from 'react';
 
 function Flashcards() {
+    const [vocab,setVocab] = useState('dog')
+
+    function handleFlip() {
+        vocab === 'dog' ? setVocab('perro') : setVocab('dog')
+    }
 
     return (
         <>
             <div>
                 <Header />
             </div>
+            <body>
+                <div className="flip-button">
+                    <div className="flip-content">
+                        <div className="flip-front" onMouseEnter={(handleFlip)}>{vocab}</div>
+                        <div className="flip-back" onMouseLeave={(handleFlip)}>{vocab}</div>
+                    </div>
+                    <button>Next Card</button>
+                </div>
+            </body>
         </>
     )
 }
